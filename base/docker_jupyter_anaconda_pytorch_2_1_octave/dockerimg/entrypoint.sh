9ac17d9697bd489a2dc89b87a924fb330bb03553
misantamaria@upm.es <mi.santamaria@upm.es>
2024-03-19 15:38:32 +0000

    conseguido docker con octave

--- base/docker_jupyter_anaconda_pytorch_2_1_octave/dockerimg/entrypoint.sh
+++ base/docker_jupyter_anaconda_pytorch_2_1_octave/dockerimg/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash --login
 set -e
-echo 'activate conda'
-conda activate $ENV_PREFIX
+echo 'activate conda $ENV_PREFIX'
+source /usr/local/share/miniconda3/bin/activate env
+
+#conda activate $ENV_PREFIX
 #echo 'about to get /home/'${USER_NAME}'/work file list'
 #ls /home/${USER_NAME}/work
 
